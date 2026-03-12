ebcbe3a4afa5b94f70d85f84a1d0ba160dd1619b
heygideon <47083178+heygideon@users.noreply.github.com>
2026-03-12 23:31:36 +0000

    add images of all featured magazine pages!

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { file } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const magazines = defineCollection({
+  loader: file("src/content/magazines.yaml"),
+  schema: ({ image }) =>
+    z.object({
+      pages: z.array(image()).min(12),
+    }),
+});
+
+export const collections = {
+  magazines,
+};
